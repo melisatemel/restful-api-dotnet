8e606c37eb5040be52c538e524684df6ea71d509
“sinanyilmaz0” <sinan.yilmaz197@gmail.com>
2023-11-05 11:26:36 +0300

    first commit

---
+++ RestfulApi/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestfulApi.Models;
+
+namespace RestfulApi.Controllers;
+
+[Route("api/products")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly List<Product> _products = new List<Product>();
+
+    [HttpGet("getAllProducts")]
+    public IActionResult GetProducts()
+    {
+        return Ok(_products);
+    }
+
+    [HttpGet("getProductById/{id}")]
+    public IActionResult GetProductById(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+            return NotFound();
+
+        return Ok(product);
+    }
+
+    [HttpPost("createProduct")]
+    public IActionResult CreateProduct([FromBody] Product newProduct)
+    {
+        if (newProduct == null)
+            return BadRequest();
+
+
+        newProduct.Id = _products.Count + 1;
+        _products.Add(newProduct);
+
+        return CreatedAtAction(nameof(GetProductById), new { id = newProduct.Id }, newProduct);
+    }
+
+    [HttpPut("updateProduct/{id}")]
+    public IActionResult UpdateProduct(int id, [FromBody] Product updatedProduct)
+    {
+        var existingProduct = _products.FirstOrDefault(p => p.Id == id);
+
+        if (existingProduct == null)
+            return NotFound();
+
+        existingProduct.Name = updatedProduct.Name;
+        existingProduct.Price = updatedProduct.Price;
+
+        return Ok(existingProduct);
+    }
+    [HttpDelete("deleteProduct/{id}")]
+    public IActionResult DeleteProduct(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+            return NotFound();
+
+        _products.Remove(product);
+
+        return NoContent(); 
+    }
+
+    [HttpGet("list")]
+    public IActionResult GetProductsByName([FromQuery] string name)
+    {
+        var filteredProducts = _products.Where(p => p.Name.Contains(name, StringComparison.OrdinalIgnoreCase)).ToList();
+        return Ok(filteredProducts);
+    }
+}
---
+++ RestfulApi/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestfulApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, 100000, ErrorMessage = "Price must be between 0.01 and 100000.")]
+        public decimal Price { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description can be at most 500 characters.")]
+        public string Description { get; set; }
+
+        [Range(0, 10000, ErrorMessage = "Stock must be between 0 and 10000.")]
+        public int Stock { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
